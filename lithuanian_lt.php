8ccf54a22214e95f401a3dbfb11b48783d2c96b3
Vilius LY3FF <vilisas@gmail.com>
2022-04-10 11:14:19 +0300

    Lithuanian translation file for pi-star

---
+++ lithuanian_lt.php
@@ -0,0 +1,150 @@
+<?php
+//
+// Lithuanian LT Language Pack
+// Vilius LY3FF
+// Updated: 2021-04-16
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "skaitmeninis balso retransliatorius",
+  "configuration"               =>  "Konfigūracija",
+  "dashboard_for"               =>  " - ",
+  // Banner links
+  "dashboard"                   =>  "Dashboard",
+  "admin"                       =>  "Valdymas",
+  "power"                       =>  "Išjungti",
+  "update"                      =>  "Atnaujinti",
+  "backup_restore"              =>  "Atsarginė kopija/Atstatyti",
+  "factory_reset"               =>  "Gamykliniai nustatymai",
+  "live_logs"                   =>  "Logai gyvai",
+  // Config page section headdings
+  "hardware_info"               =>  "Gateway Hardware informacija",
+  "control_software"            =>  "Valdymo programos",
+  "mmdvmhost_config"            =>  "MMDVMHost konfigūracija",
+  "general_config"              =>  "Pagrindine konfigūracija",
+  "dmr_config"                  =>  "DMR konfigūracija",
+  "dstar_config"                =>  "D-Star konfigūracija",
+  "ysf_config"                  =>  "Yaesu System Fusion konfigūracija",
+  "p25_config"                  =>  "P25 konfigūracija",
+  "nxdn_config"                 =>  "NXDN konfigūracija",
+  "pocsag_config"               =>  "POCSAG konfigūracija",
+  "mobilegps_config"            =>  "Mobile GPS konfigūracija",
+  "wifi_config"                 =>  "Wireless konfigūracija",
+  "fw_config"                   =>  "Firewall konfigūracija",
+  "remote_access_pw"            =>  "Nuotolinės prieigos slaptažodis",
+  // Config Page - Section General
+  "setting"                     =>  "Parametras",
+  "value"                       =>  "Reikšmė",
+  "apply"                       =>  "Taikyti pakeitimus",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Hostname",
+  "kernel"                      =>  "Branduolys",
+  "platform"                    =>  "Platforma",
+  "cpu_load"                    =>  "CPU Apkrova",
+  "cpu_temp"                    =>  "CPU Temperatūra",
+  // Config Page - Control Software
+  "controller_software"         =>  "Valdiklio programa",
+  "controller_mode"             =>  "Valdiklio moda",
+  // Config Page - MMDVMHost konfigūracija
+  "dmr_mode"                    =>  "DMR Moda",
+  "d-star_mode"                 =>  "D-Star Moda",
+  "ysf_mode"                    =>  "YSF Moda",
+  "p25_mode"                    =>  "P25 Moda",
+  "nxdn_mode"                   =>  "NXDN Moda",
+  "mmdvm_display"               =>  "MMDVM ekrano tipas",
+  "mode_hangtime"               =>  "Mode Hangtime",
+  // Config Page - General konfigūracija
+  "node_call"                   =>  "Nodo šaukinys",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "Radijo dažnis",
+  "lattitude"                   =>  "Latitude",
+  "longitude"                   =>  "Longitude",
+  "town"                        =>  "Miestas",
+  "country"                     =>  "Šalis",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Radio/Modemo tipas",
+  "node_type"                   =>  "Nodo Typas",
+  "timezone"                    =>  "Sistemos laiko zona",
+  "dash_lang"                   =>  "Dashboard kalba",
+  // Config Page - DMR konfigūracija
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Master",
+  "bm_network"                  =>  "BrandMeister Network",
+  "dmr_plus_master"             =>  "DMR+ Master",
+  "dmr_plus_network"            =>  "DMR+ Network",
+  "xlx_master"                  =>  "XLX Master",
+  "xlx_enable"                  =>  "XLX Master Enable",
+  "dmr_cc"                      =>  "DMR Color Code",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star konfigūracija
+  "dstar_rpt1"                  =>  "RPT1 Callsign",
+  "dstar_rpt2"                  =>  "RPT2 Callsign",
+  "dstar_irc_password"          =>  "Remote Password",
+  "dstar_default_ref"           =>  "Default Reflector",
+  "aprs_host"                   =>  "APRS Host",
+  "dstar_irc_lang"              =>  "ircDDBGateway Language",
+  "dstar_irc_time"              =>  "Time Announcements",
+  // Config Page - YSF konfigūracija
+  "ysf_startup_host"            =>  "YSF Startup Host",
+  // Config Page - P25 konfigūracija
+  "p25_startup_host"            =>  "P25 Startup Host",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - NXDN konfigūracija
+  "nxdn_startup_host"            =>  "NXDN Startup Host",
+  "nxdn_ran"                     =>  "NXDN RAN",
+  // Config Page - MobileGPS konfigūracija
+  "mobilegps_enable"            =>  "MobileGPS Enable",
+  "mobilegps_port"              =>  "GPS Portas",
+  "mobilegps_speed"             =>  "GPS Porto greitis",
+  // Config Page - Firewall konfigūracija
+  "fw_dash"                     =>  "Dashboard prieiga",
+  "fw_irc"                      =>  "ircDDBGateway Remote",
+  "fw_ssh"                      =>  "SSH prieiga",
+  // Config Page - Password
+  "user"                        =>  "User Name",
+  "password"                    =>  "Slaptažodis",
+  "set_password"                =>  "Nustatyti slaptažodį",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Įjungtos modos",
+  "net_status"                  =>  "Tinklo būsena",
+  "internet"                    =>  "Internetas",
+  "radio_info"                  =>  "Radijo Info",
+  "dstar_repeater"              =>  "D-Star Retransliatorius",
+  "dstar_net"                   =>  "D-Star Tinklas",
+  "dmr_repeater"                =>  "DMR Retransliatorius",
+  "dmr_master"                  =>  "DMR Master",
+  "ysf_net"                     =>  "YSF Tinklas",
+  "p25_radio"                   =>  "P25 Radijas",
+  "p25_net"                     =>  "P25 Tinklas",
+  "nxdn_radio"                  =>  "NXDN Radijas",
+  "nxdn_net"                    =>  "NXDN Tinklas",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Laikas",
+  "mode"                        =>  "Moda",
+  "callsign"                    =>  "Šaukinys",
+  "target"                      =>  "Koresp.",
+  "src"                         =>  "Šalt",        // Short version of "Source"
+  "dur"                         =>  "Trukmė",        // Short version of "Duration"
+  "loss"                        =>  "Loss",
+  "ber"                         =>  "BER",        // Short version of "Bit Error Rate"
+  // POCSAG Specific
+  "pocsag_list"                 =>  "DAPNET Gateway aktyvumas",
+  "pocsag_timeslot"             =>  "Time Slot",
+  "pocsag_msg"                  =>  "Pranešimas",
+  // Dashboard - Extra Info
+  "group"                       =>  "Grupė",
+  "logoff"                      =>  "Atsijungti",
+  "info"                        =>  "Informacija",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Gateway aktyvumas",
+  "local_tx_list"               =>  "Vietinis RF aktyvumas",
+  "active_starnet_groups"       =>  "Active Starnet Groups",
+  "active_starnet_members"      =>  "Active Starnet Group Members",
+  "d-star_link_manager"         =>  "D-Star Link Manager",
+  "d-star_link_status"          =>  "D-Star Link Information",
+  "service_status"              =>  "Servisų Būsena"
+);
+?>
